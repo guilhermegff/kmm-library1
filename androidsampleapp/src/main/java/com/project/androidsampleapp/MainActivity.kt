7fe0edeb1255102e4fa864eccb059dfb37f75f3c
Guilherme Freire <gfreire@MB.local>
2023-09-22 13:58:41 -0300

    Call kmm lib from sample app

--- androidsampleapp/src/main/java/com/project/androidsampleapp/MainActivity.kt
+++ androidsampleapp/src/main/java/com/project/androidsampleapp/MainActivity.kt
@@ -3,16 +3,21 @@ package com.project.androidsampleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.project.androidsampleapp.ui.theme.Kmm_library1Theme
+import com.project.kmm_library1.KmmLibrary1Sdk
 
 class MainActivity : ComponentActivity() {
+    private val kmmLib = KmmLibrary1Sdk()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +26,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.padding(vertical = 30.dp)
+                    ) {
+                        Greeting("Android")
+                        Greeting(kmmLib.getNumber5().toString())
+                    }
                 }
             }
         }
